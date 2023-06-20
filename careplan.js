bd5b513f1bc13adb92d128f60e718768ced00a39
Tonye Brown <tonye.brown@vorihealth.com>
2023-06-20 16:08:55 -0700

    increase version

--- careplan.js
+++ careplan.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Healthie Care Plan Integration
 // @namespace    http://tampermonkey.net/
-// @version      0.28
+// @version      0.29
 // @description  Injecting care plan components into Healthie
 // @author       Don, Tonye
 // @match        https://*.gethealthie.com/*
