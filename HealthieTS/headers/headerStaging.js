0e8a1d2ce72502970dc70d8aa69f9532ea2f056c
Alejandro Oyuela <alejandro.oyuela@vorihealth.com>
2023-11-27 08:15:24 -0500

    CAX-72-TM-typescript-refactor

---
+++ HealthieTS/headers/headerStaging.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Healthie Care Plan Integration Staging
+// @namespace    http://tampermonkey.net/
+// @version      0.71
+// @description  Injecting care plan components into Healthie
+// @author       Don, Tonye, Alejandro
+// @match        https://*.gethealthie.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=vori.health
+// @sandbox      JavaScript
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
---
+++ HealthieTS/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const header = fs.readFileSync(path.resolve(__dirname, 'headers/header.js'), 'utf8');
+const headerStaging = fs.readFileSync(path.resolve(__dirname, 'headers/headerStaging.js'), 'utf8');
+module.exports = [
+  {
+    // Your existing configuration for careplan.js
+    mode: 'development', // or 'production'
+    entry: './careplan.ts',
+    output: {
+      path: path.resolve(__dirname, 'TM files'),
+      filename: 'careplan.js'
+    },
+    plugins: [
+      new webpack.BannerPlugin({
+        banner: header,
+        raw: true,
+        entryOnly: true
+      }),
+    ],
+    module: {
+      rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ }],
+    },
+    resolve: { extensions: ['.tsx', '.ts', '.js'] },
+  },
+  {
+    // Configuration for careplanstaging.js
+    mode: 'development', // or 'production'
+    entry: './careplan.ts',
+    output: {
+      path: path.resolve(__dirname, 'TM files'),
+      filename: 'careplanstaging.js'
+    },
+    plugins: [
+      new webpack.BannerPlugin({
+        banner: headerStaging,
+        raw: true,
+        entryOnly: true
+      }),
+    ],
+    module: {
+      rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ }],
+    },
+    resolve: { extensions: ['.tsx', '.ts', '.js'] },
+  }
+];
+
+
+
+
