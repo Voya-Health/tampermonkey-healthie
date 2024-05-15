6c61b21ce3c5d8c99cc3ca02d95ff9cd85977abf
Donald Heatherington <71086843+dheatherington@users.noreply.github.com>
2024-05-15 12:41:28 -0400

    CAX-581-fix-onboarding-status (#89)

--- careplan.js
+++ careplan.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Healthie Care Plan Integration
 // @namespace    http://tampermonkey.net/
-// @version      0.78
+// @version      0.79
 // @description  Injecting care plan components into Healthie
 // @author       Don, Tonye, Alejandro
 // @match        https://*.gethealthie.com/*
@@ -1472,7 +1472,7 @@ function healthieGQL(payload) {
 
 function addMembershipAndOnboarding() {
   //get phone icon and related column
-  const phoneColumn = document.querySelector(".col-12.col-sm-6:has(.telephone-icon)");
+  const phoneColumn = document.querySelector('#main-section-container > div > div > div > div.col-3.col-sm-12.client-profile-sidebar > div:nth-child(1) > div:nth-child(1) > div > section:nth-child(2) > div.BasicInfo_basicInfo__Ks2nG > div > div:nth-child(1)')
   const iframeAdded = phoneColumn ? phoneColumn.parentNode.querySelector(".misha-iframe-container") : null;
 
   if (phoneColumn && !iframeAdded) {
