26cdbabcabade22663d0ce4297d3efe1a31addaa
Donald Heatherington <71086843+dheatherington@users.noreply.github.com>
2024-12-30 11:55:56 -0500

    CAX-1063-add-back-insurance type

--- careplan.js
+++ careplan.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Healthie Care Plan Integration
 // @namespace    http://tampermonkey.net/
-// @version      0.87
+// @version      0.88
 // @description  Injecting care plan components into Healthie
 // @author       Don, Tonye, Alejandro
 // @match        https://*.gethealthie.com/*
@@ -397,7 +397,7 @@ function hideGroupNameOccurrences() {
 
   const selectors = {
     sidebarGroup: 'section[data-testid="cp-section-basic-information"] .row > .col-12:nth-child(3)',
-    clientInfoGroup: 'div[data-testid="collapsible-section-body"] .row > .col-6:nth-of-type(1)',
+    clientInfoGroup: '#main-section-container > div > div > div > div.col-9.col-sm-12 > div.cp-tab-contents > div.ActionsTabClientForms_formsContainer__3qLl5 > div:nth-child(1) > div.CollapsibleSection_healthieCollapsibleSection__2qKEm.CollapsibleSection_clipContent__uYSyD > div.CollapsibleSection_sectionBody__5VeiC > form > div:nth-child(7) > div:nth-child(2)',
     groupTab: "div#tab-groups",
     tableColumns: ".all-users table.table.users-table.users-list tr > *:nth-child(6)",
   };
