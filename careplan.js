d067b1bdb6d0ac4815084e65dcbffe441d3b46ec
Tonye Brown <tonye.brown@vorihealth.com>
2025-02-18 15:55:38 -0800

    Update iframe dimensions for patientstatusstandalone

--- careplan.js
+++ careplan.js
@@ -1623,7 +1623,11 @@ function addMembershipAndOnboarding() {
     debugLog(`tampermonkey patient number`, patientNumber);
     // create iframe (generateIframe returns a jQuery object)
     //Add custom height and width to avoid scrollbars because the material ui Select component
-    const iframe = generateIframe(`${routeURLs.patientStatus}/${patientNumber}`, { height: "190px", width: "400px" });
+    const iframe = generateIframe(`${routeURLs.patientStatus}/${patientNumber}`, {
+      height: "322px",
+      width: "105%",
+      minWidth: "280px",
+    });
     const iframeExists = phoneColumn.parentNode.querySelector(".misha-iframe-container");
     // add iframe after phone element, get the native DOM Node from the jQuery object, this is the first array element.
     !iframeExists && phoneColumn.parentNode.insertBefore(iframe[0], phoneColumn.nextSibling);
