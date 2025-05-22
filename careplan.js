264555c83d2016e0c1da2ebbc57567681933943b
Oksana Miller <152429450+oksanaMillerVori@users.noreply.github.com>
2025-05-22 14:49:43 -0400

    CAX-1328
    add pediatric label

--- careplan.js
+++ careplan.js
@@ -1032,6 +1032,63 @@ function waitGoalTab() {
   }
 }
 
+function isPediatric(dobString) {
+  const dob = new Date(dobString);
+  const today = new Date();
+  let age = today.getFullYear() - dob.getFullYear();
+
+  // Adjust if birthday hasn't occurred yet this year
+  const hasBirthdayPassed = 
+    today.getMonth() > dob.getMonth() || 
+    (today.getMonth() === dob.getMonth() && today.getDate() >= dob.getDate());
+
+  if (!hasBirthdayPassed) {
+    age--;
+  }
+  return age < 18;
+}
+
+function loadPediatricBanner() {
+  // find patient DOB
+  const $ = initJQuery();
+  if (!$) {
+    debugLog(`tampermonkey waiting for jquery to load`);
+    createTimeout(loadPediatricBanner, 200);
+  } else {
+    const basicInfo = $('.BasicInfo_basicInfo__Ks2nG');
+    if (basicInfo.length > 0) {
+        debugLog(`TEST _>>>> BASIC info loaded`);
+        const dob = $('[data-testid="client-dob"]').text();
+        if (dob.length > 0) {
+          debugLog(`TEST _>>>> dob loaded`, dob);
+          const isPatientPediatric = isPediatric(dob);
+          //const pediatricBanner = $('.pediatric-banner');
+          debugLog(`TEST _>>>> isPatientPediatric`,isPatientPediatric);
+
+          if (isPatientPediatric) {
+            // insert pediatric label
+            const searchBarHeader = $('#main-layout__header');
+            $('<div>PEDIATRIC</div>').css({
+                backgroundColor: '#EDF4FB',
+                color: '#457AC8',
+                fontWeight: '700',
+                marginTop: '60px',
+                padding: '12px 24px'
+              }).insertAfter(searchBarHeader);
+          }
+
+
+      } else {
+        //wait for content load
+        createTimeout(loadPediatricBanner, 200);
+      }
+    } else {
+      //wait for content load
+      createTimeout(loadPediatricBanner, 200);
+    }
+  }
+}
+
 function waitCarePlan() {
   const $ = initJQuery();
   if (!$) {
@@ -1807,6 +1864,8 @@ function observeDOMChanges(mutations, observer) {
       //Function that will check when goal tab has loaded
       debugLog("tampermonkey calls waitGoalTab");
       waitGoalTab();
+      debugLog("tampermonkey calls loadPediatricBanner");
+      loadPediatricBanner();
     }
 
     if (urlValidation.appointmentsProfile.test(location.href)) {
