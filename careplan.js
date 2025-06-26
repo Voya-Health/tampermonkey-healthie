2dcdad809302090021735473276a2dcfb4f9ae61
Donald Heatherington <71086843+dheatherington@users.noreply.github.com>
2025-06-26 11:06:36 -0400

    CAX-1415-fix-pediatric-banner (#119)
    * CAX-1415-fix-pediatric-banner
    
    * update version

--- careplan.js
+++ careplan.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Healthie Care Plan Integration
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Injecting care plan components into Healthie
 // @author       Don, Tonye, Alejandro
 // @match        https://*.gethealthie.com/*
@@ -1058,7 +1058,7 @@ function loadPediatricBanner() {
     debugLog(`tampermonkey waiting for jquery to load`);
     createTimeout(loadPediatricBanner, 200);
   } else {
-    const basicInfo = $(".BasicInfo_basicInfo__Ks2nG");
+    const basicInfo = $('[data-testid="cp-section-basic-information"]');
     if (basicInfo.length > 0) {
       const dob = $('[data-testid="client-dob"]').text();
       if (dob.length > 0) {
