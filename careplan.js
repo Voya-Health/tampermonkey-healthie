a5ee90696a949b8fc87ea612ecb4abeab5c1ca73
Donald Heatherington <71086843+dheatherington@users.noreply.github.com>
2025-06-26 10:58:43 -0400

    update version

--- careplan.js
+++ careplan.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Healthie Care Plan Integration
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Injecting care plan components into Healthie
 // @author       Don, Tonye, Alejandro
 // @match        https://*.gethealthie.com/*
